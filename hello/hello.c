df7b0fb4467cb54e7309fe4082df4c72eedcd389
houminwei <houminwei@tencent.com>
2021-03-25 21:10:11 +0800

    Feature: kernel module Hello World!

---
+++ hello/hello.c
@@ -0,0 +1,19 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+static int hello_init(void)
+{
+    printk(KERN_ALERT "Hello, world!\n");
+    return 0;
+}
+
+static void hello_exit(void)
+{
+    printk(KERN_ALERT "GoodBye, cruel world!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+MODULE_AUTHOR("houmin.wei@outlook.com");
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_DESCRIPTION("A Hello, world Module");
